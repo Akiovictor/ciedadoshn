0b03578f3c5224633f01d4981409c305ecc7ad50
Akiovictor <victor.ay@hotmail.com>
2024-11-05 20:06:20 -0300

    alterei .js

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -5,12 +5,17 @@ async function vizualizarInformacoesGlobais() {
     const dados = await res.json()
 
     const PessoasNoMundo = (dados.total_pessoas_mundo / 1e9)
+    const PessoasQueTrabalham = (dados.total_trabalhadores_mundo / 1e9)
+    const PessoasNecessitamTransporte = (dados.total_trabalhadores_mundo / 1e9)
+    const TempoDeDeslocamento = (dados.tempo_medio_deslocamento_para_trabalho)
 
+    const horas = parseInt(dados.tempo_medio_deslocamento_para_trabalho)
+    const minutos = Math.round((dados.tempo_medio_deslocamento_para_trabalho - horas) * 60)
 
     console.log(dados)
     const paragrafo = document.createElement('p')
     paragrafo.classList.add('graficos-container__texto')
-    paragrafo.innerHTML = `No mundo com <span>${PessoasNoMundo} </span> bilhões de pessoas, cerca de <span>${dados.total_pessoas_que_necessitam_transporte_para_trabalho} </span> fazem parte da força de trabalho global, e <span>${dados.tempo_medio_deslocamento_para_trabalho} </span> delas dependem de transporte para chegar ao trabalho. Isso significa que, apenas <pan>38% (=3/7,888) </span>no mundo tem trabalho. Do total de pessoas no mundo <span>25% </span>utilizam transporte e dos que trabalham 67% precisam de transporte. E dos que utilizam transporte, estes gastam <span>3,75% (0,9/24) </span> do tempo do dia para deslocamento. Esse tempo, embora pareça curto, simboliza a resiliência e busca por melhores oportunidades.
+    paragrafo.innerHTML = `No mundo com <span> ${PessoasNoMundo} </span> bilhões de pessoas, cerca de <span>${PessoasQueTrabalham} </span> fazem parte da força de trabalho global, e <span>${PessoasNecessitamTransporte} </span> delas dependem de transporte para chegar ao trabalho. Isso significa que, apenas <pan>38% (=3/7,888) </span>no mundo tem trabalho. Do total de pessoas no mundo <span>25% </span>utilizam transporte e dos que trabalham 67% precisam de transporte. E dos que utilizam transporte, estes gastam <span>3,75% (0,9/24) </span> do tempo do dia para deslocamento. Esse tempo, embora pareça curto, simboliza a resiliência e busca por melhores oportunidades.
 `
     const container = document.getElementById('graficos-container')
     container.appendChild(paragrafo);
