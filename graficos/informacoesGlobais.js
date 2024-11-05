7a9b5b51b1a7f8878d4bc421f5343f9c740d3ed4
Akiovictor <victor.ay@hotmail.com>
2024-11-05 20:13:59 -0300

    dados colocados

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -12,10 +12,15 @@ async function vizualizarInformacoesGlobais() {
     const horas = parseInt(dados.tempo_medio_deslocamento_para_trabalho)
     const minutos = Math.round((dados.tempo_medio_deslocamento_para_trabalho - horas) * 60)
 
+    const PorcentagemDeTrabalhadores = ((PessoasQueTrabalham / PessoasNoMundo ) * 100).toFixed(2)
+    const PorcentagemTransporteDoMundo = ((PessoasNecessitamTransporte / PessoasNoMundo ) * 100).toFixed(2)
+    const PorcentagemTansporteDosTrabalham = ((PessoasNecessitamTransporte / PessoasNoMundo ) * 100).toFixed(2)
+
+
     console.log(dados)
     const paragrafo = document.createElement('p')
     paragrafo.classList.add('graficos-container__texto')
-    paragrafo.innerHTML = `No mundo com <span> ${PessoasNoMundo} </span> bilhões de pessoas, cerca de <span>${PessoasQueTrabalham} </span> fazem parte da força de trabalho global, e <span>${PessoasNecessitamTransporte} </span> delas dependem de transporte para chegar ao trabalho. Isso significa que, apenas <pan>38% (=3/7,888) </span>no mundo tem trabalho. Do total de pessoas no mundo <span>25% </span>utilizam transporte e dos que trabalham 67% precisam de transporte. E dos que utilizam transporte, estes gastam <span>3,75% (0,9/24) </span> do tempo do dia para deslocamento. Esse tempo, embora pareça curto, simboliza a resiliência e busca por melhores oportunidades.
+    paragrafo.innerHTML = `No mundo com <span> ${PessoasNoMundo} </span> bilhões de pessoas, cerca de <span>${PessoasQueTrabalham} </span> fazem parte da força de trabalho global, e <span>${PessoasNecessitamTransporte} </span> delas dependem de transporte para chegar ao trabalho. Isso significa que, apenas <pan>${PorcentagemDeTrabalhadores}%</span>no mundo tem trabalho. Do total de pessoas no mundo <span> ${PorcentagemTransporteDoMundo}% </span> utilizam transporte e dos que trabalham <span>${PorcentagemTansporteDosTrabalham}</span> precisam de transporte. E dos que utilizam transporte, estes gastam <span> ${TempoDeDeslocamento} </span> do tempo do dia para deslocamento. Esse tempo, embora pareça curto, simboliza a resiliência e busca por melhores oportunidades.
 `
     const container = document.getElementById('graficos-container')
     container.appendChild(paragrafo);
